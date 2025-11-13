f0fe50cd1f8e0bf8b5d1cac509bfc63ef48c670c
Samuel <lakamosamuel7@gmail.com>
2025-11-13 12:08:09 +0100

    Add: make the app installable with pwa

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,18 @@ import App from './App.tsx'
 import { DarkModeProvider } from './context/DarkModeContext.tsx'
 import { SidebarProvider } from './context/SidebarToggleContext.tsx'
 
+import { registerSW } from "virtual:pwa-register";
+
+const updateSW = registerSW({
+  onNeedRefresh() {
+    if (confirm("New content available. Reload?")) {
+      updateSW(true);
+    }
+  },
+  onOfflineReady() {
+    console.log("App ready to work offline 🚀");
+  },
+});
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <DarkModeProvider>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,42 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate", // auto updates service worker
+      includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
+      manifest: {
+        name: "Chronovah",
+        short_name: "Chronovah",
+        description: "All your life details in one place",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#000000",
+        theme_color: "#2b6cb0", 
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+          {
+            src: "/favicon-32x32.png",
+            sizes: "32x32",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+      },
+    }),
+  ],
+});
